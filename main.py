d7f537eeb984b702917b9c9a6362ea0780788f51
Ben Asselmeier <benasselmeier@gmail.com>
2018-04-08 18:00:30 -0500

    works, only thing left is to have the form retain information when submitted.

--- main.py
+++ main.py
@@ -5,20 +5,44 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
-@app.route("/welcome", methods=['POST'])
+@app.route("/signup", methods=['POST'])
 def signup():
 
     username = request.form['username']
     password = request.form['password']
     confirm = request.form['confirm']
     email = request.form['email']
-    error = ""
 
-    if len(username) > 3 and len(username) <20:
-        return render_template('welcome.html', username=username)
+    if len(username) >= 3 and len(username) <= 20 and " " not in username:
+        username=username
+        username_error = ""
     else:
-        error = "invalid username"
-        return render_template('form-signup.html', error=error)
+        username_error = "invalid username"
+
+    if len(password) >= 3 and len(password) <= 20 and " " not in password:
+        password=password
+        password_error = ""
+    else:
+        password_error = "invalid password"
+    
+    if confirm == password:
+        confirm=confirm
+        confirm_error = ""
+    else:
+        confirm_error = "passwords don't match"
+
+    if email == "" or (len(email) >= 3 and len(email) <= 20) and ('.' in email and '@' in email) and " " not in email:
+        email = email
+        email_error = ""
+    else:
+        email_error = "invalid email"
+
+    rules = (username_error == "", password_error == "", confirm_error == "", email_error == "")
+
+    if all(rules):
+        return render_template('welcome.html', username = username)
+    else:
+        return render_template('form-signup.html', username=username, username_error=username_error, password_error=password_error, confirm_error = confirm_error, email_error = email_error)
 
 @app.route("/")
 def index():
