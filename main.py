2055585ae8d7c7ee12e77c64301ad47de7a498e4
Ben Asselmeier <benasselmeier@gmail.com>
2018-04-07 19:41:47 -0500

    holy shit i'm exhausted and hospital wifi is slow pls send help and/or caffeine

--- main.py
+++ main.py
@@ -5,6 +5,21 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
+@app.route("/signup", methods=['POST'])
+def signup():
+
+    username = request.form['username']
+    password = request.form['password']
+    confirm = request.form['confirm']
+    email = request.form['email']
+    error = ""
+
+    if (len(username) < 3) or (len(username) > 20):
+        error = "invalid username"
+        return redirect("/?error=" + error)
+
+    render_template('index.html')
+
 @app.route("/")
 def index():
     return render_template('form-signup.html')
